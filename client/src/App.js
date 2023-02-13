d2e7af7e3aa5b29435a647f0497736251c8b9a5f
Zain <zat938@gmail.com>
2023-02-13 17:54:49 +0500

    if applied, this commit bootstrap the node.js and react.js project together and test the frontend by starting the server

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchClinic from "./components/SearchClinics";
+
+function App() {
+  return (
+    <div>
+      <SearchClinic />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.json({ hey: "World!" });
+});
+
+app.get("/clinics", async (req, res) => {
+  res.send({ clinics: [{ name: "dummy clinic" }] });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
