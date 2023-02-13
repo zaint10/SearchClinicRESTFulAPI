06a7d989bd109c5d541500530b71692e1d3d1f54
Zain <zat938@gmail.com>
2023-02-13 21:26:17 +0500

    if applied, thhis commit adds the test and fix some bug

--- server/server.js
+++ server/server.js
@@ -130,3 +130,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+module.exports = app;
--- server/test/api.test.js
+++ server/test/api.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../server");
+const app = require("../server.js");
 
 describe("GET /api", () => {
   it("should return an array of clinics", async () => {
